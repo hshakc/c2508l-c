9b17bd9233ce4db35c2dfcd18d62cc1950f7c888
hshakc <vut33447@gmail.com>
2025-11-24 21:36:44 +0700

    Add files via upload

---
+++ buoi2.c
@@ -0,0 +1,15 @@
+// Comment nghia la khong cho trinh bien dich dich dong code nay
+// Day la tien xu ly (proprocessor). Muc dich la nhung thu vien
+// Standard Input Output ( thu vien nhap xuat chuan)
+// vao trong file thuc thi
+#include <stdio.h>
+ 
+ // Day la ham ma chuong trinh se thuc thi dau tien
+int main(){
+	// day la than ham
+	
+	// Ham printf ( print formatted) dung de in ra man hinh mot cai gi do
+	printf("Xin chao");
+	
+	return 0;
+}
