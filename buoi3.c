419f72cc243e773ec62636ce78a767b11eb5eeb7
hshakc <vut33447@gmail.com>
2025-11-29 05:59:58 +0700

    Add files via upload

---
+++ buoi3.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+int main(){
+	
+	// khai bao bien variable khai bao bien
+	// nghia la tao 1 bien ma khong gan gia tri
+	//cu phap: syntax: kieu_du_lieu ten_bien;
+//	
+//	int a = 5;
+//	float b = 8.9;
+//	double c = 9.555555;
+//	char d = 'A';
+	
+	// printf  la ham in du lieu ra man hinh
+	// scanf la nhap du lieu tu ban phim
+	// initialaize variable ( khoi tao bien)
+	// nghia la tao 1 bien va gan gia tri cho bien do
+	
+	// yeu cau nguoi dung nhap so a, so b sau do in ra man hinh ket qua cua a-b
+	
+//	int a;
+//	int b;
+//	
+//	printf ("nhap so a");
+//	scanf ("%d",&a);
+//	
+//	printf ("nhap so b");
+//	scanf ("%d", &b);
+//	
+//	printf ("tong cua a va b la: %d", a+b);
+
+
+//	float a;
+//	float b;
+//	
+//	printf ("nhap so a");
+//	scanf ("%f", &a);
+//	
+//	printf ("nhap so b");
+//	scanf ("%f", &b);
+//	
+//	printf ("a chia b = %f", a/b);
+//	
+	
+//	int a, b;
+//	
+//	printf ("Nhap so a: ");
+//	scanf ("%d", &a);
+//	printf ("nhap so b: ");
+//	scanf ("%d", &b);
+//	printf ("a / b = %.1f", (float)a / b);
+//	
+//	printf("\n");
+//	printf ("Gia tri cua bien a la: %d \n",a);
+//	printf("Dia chi cua bien a trong RAM la: %p", &a);
+//
+//
+//
+
+//	int a = 1, b =2;
+//	
+//
+//	
+//	printf ("%d chia lay du cho %d = %d", a, b, a % b );
+//	int a = 5;
+//	printf ("Gia tri cua bien a la: %d\n", a);
+// 
+////	a = a + 1;
+//	printf ("Gia tri cua bien a sau thay doi: %d\n", a++);
+//	printf ("Gia tri cua bien a la: %d", a);
+////	
+//	int a = 5;
+////	a = ++a + 1 + a++;
+//	a = --a + 1 + a--;
+//	printf ("Gia tri cua a la: %d", a);
+//	
+//	a == 5;
+
+
+//	int a =5; 
+//	printf ("a +=5 la: %d",a +=5);
+//	printf ("a -=5 la: %d\n",a -=5);
+//	printf ("a *=5 la: %d\n",a *=5);
+//	printf ("a /=5 la: %d\n",a /=5);
+//	printf ("a chia lay du cho 5 la: %d\n",a %=5);
+
+// 0 la false , 1 la true 
+	int x = 5, y =3;
+	printf ("x > y: %d\n", x > y);
+	printf ("x < y: %d\n", x < y);
+	printf ("x == y: %d\n", x == y);
+	printf ("x >= y: %d\n", x >= y);
+	printf ("x <= y: %d\n", x <= y);
+	printf ("x != y: %d\n", x != y);
+	return 0;
+}
